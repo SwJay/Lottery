c1c271df011ee48e90939ebbfd3d20e28f778beb
JosephSun <sunweijay@outlook.com>
2018-11-20 15:59:32 +0800

    Update truffle.js
    delete private key

--- my-app/truffle.js
+++ my-app/truffle.js
@@ -14,9 +14,7 @@
 
 var WalletProvider = require("truffle-wallet-provider");
 const Wallet = require('ethereumjs-wallet');
-// var ropstenPrivateKey = new Buffer("063DB0C2D18DF840B31BAFBC871FA598C81F558244EF621C90955B05813A9163","hex");
 // var ropstenWallet = Wallet.fromPrivateKey(ropstenPrivateKey);
-// var ropstenProvider = new WalletProvider(ropstenWallet, "https://ropsten.infura.io/v3/ca26ad0352634c82948dca4688a4557b");
 
 module.exports = {
   networks: {
