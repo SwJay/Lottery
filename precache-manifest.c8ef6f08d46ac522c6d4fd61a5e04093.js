a521496054229da3ba3470837fa7d04912b87d56
Virgil Sun <swj@Virgils-MacBook-Pro.local>
2019-05-20 15:41:37 +0800

    Updates

---
+++ precache-manifest.c8ef6f08d46ac522c6d4fd61a5e04093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25015cd59391cdc7cb93a909f6e0ff0",
+    "url": "/Lottery/index.html"
+  },
+  {
+    "revision": "dd74335759c8913be266",
+    "url": "/Lottery/static/css/main.7e3f6520.chunk.css"
+  },
+  {
+    "revision": "f896e0a28d4be7532a4f",
+    "url": "/Lottery/static/js/2.db8ad3d7.chunk.js"
+  },
+  {
+    "revision": "dd74335759c8913be266",
+    "url": "/Lottery/static/js/main.052ada12.chunk.js"
+  },
+  {
+    "revision": "936db1a6b4a5746d4ee3",
+    "url": "/Lottery/static/js/runtime~main.99e1bf7c.js"
+  },
+  {
+    "revision": "6f9316c1a47e5e19a7ca6f1b3cb15aec",
+    "url": "/Lottery/static/media/logo.6f9316c1.svg"
+  }
+]);
